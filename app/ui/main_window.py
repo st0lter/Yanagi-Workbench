d17940c0bd61a67b8e4d93484e10ba625b3b2a0b
Tony Denalar <196391699+st0lter@users.noreply.github.com>
2026-08-19 19:40:52 -0300

    UPDATE: Inserts OptionFrame into main_window.

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -1,5 +1,14 @@
 import customtkinter as ctk
-from app.ui.options_frame import OptionsFrame
+
+class OptionsFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.columnconfigure(0, weight=1)
+
+        self.title = ctk.CTkLabel(self, text='Yanagi Workbench')
+        self.title.grid(row=0, column=0, padx=12, pady=12, sticky='nsew')
+
 
 class App(ctk.CTk):
     def __init__(self):
@@ -7,3 +16,6 @@ class App(ctk.CTk):
 
         self.options_frame = OptionsFrame(self)
         self.options_frame.grid(row=0, column=0)
+
+        self.main_task_frame = MainTaskFrame(self)
+        self.main_task_frame.grid(row=0, column=1)
