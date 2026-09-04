2a012580b5b59e6320924a37514f922becb9aea9
Tony Denalar <196391699+st0lter@users.noreply.github.com>
2026-09-04 14:53:18 -0300

    ADD: Add font configuration in config folder.

---
+++ app/config/__init__.py
@@ -0,0 +1,5 @@
+FONTS = {
+    "default": ("Helvetica", 12),
+    "bold": ("Helvetica", 12, "bold"),
+    "italic": ("Helvetica", 12, "italic")
+}
--- app/ui/home_page.py
+++ app/ui/home_page.py
@@ -1,4 +1,5 @@
 import ttkbootstrap as ttk
+from app.config import FONTS
 
 class HomePage(ttk.Frame):
     def __init__(self, parent):
@@ -7,5 +8,12 @@ class HomePage(ttk.Frame):
 
     def _create_widgets(self):
         # Create home-specific widgets
-        self.label = ttk.Label(self, text="Welcome to Yanagi Workbench!", font=("Helvetica", 14))
-        self.label.grid(row=0, column=0, pady=10, padx=10)
+        ttk.Label(self, text="Welcome to Yanagi Workbench!", font=FONTS["bold"]).grid(row=0, column=0, pady=10, padx=10)
+
+        ttk.Label(self, text="This is the home page of the application.", font=FONTS["default"]).grid(row=1, column=0, pady=5, padx=10)
+
+        ttk.Label(self,
+                  text="Use the navigation buttons on the left to access different features of the application.",
+                  font=FONTS["default"]).grid(row=2, column=0, pady=5, padx=10)
+
+        ttk.Label(self, text="Enjoy your experience!", font=FONTS["italic"]).grid(row=3, column=0, pady=5, padx=10)
