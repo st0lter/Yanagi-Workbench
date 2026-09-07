9ec21543b44163bee7922890ed359ff3ab233ab7
Tony Denalar <196391699+st0lter@users.noreply.github.com>
2026-09-06 23:15:37 -0300

    UPDATE: Create structure for file management and image handling.

---
+++ app/ui/file_page.py
@@ -0,0 +1,19 @@
+import ttkbootstrap as ttk
+from app.config import FONTS
+
+class FilePage(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self._create_widgets()
+
+    def _create_widgets(self):
+        # Create file-specific widgets
+        ttk.Label(self, text="Welcome to the File Page!", font=FONTS["bold"]).grid(row=0, column=0, pady=10, padx=10)
+
+        ttk.Label(self, text="This is the file page of the application.", font=FONTS["default"]).grid(row=1, column=0, pady=5, padx=10)
+
+        ttk.Label(self,
+                  text="Use the navigation buttons on the left to access different features of the application.",
+                  font=FONTS["default"]).grid(row=2, column=0, pady=5, padx=10)
+
+        ttk.Label(self, text="Enjoy your experience!", font=FONTS["italic"]).grid(row=3, column=0, pady=5, padx=10)
